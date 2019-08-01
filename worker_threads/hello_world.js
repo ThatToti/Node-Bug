d402d5836718cf80a8977edc60c6955c8b5cd404
ThatToti <mr.totihuang@gmail.com>
2019-08-01 21:18:44 +0800

    【+】hello_world demo

---
+++ worker_threads/hello_world.js
@@ -0,0 +1,14 @@
+
+
+const {Worker,isMainThread,parentPort}=require('worker_threads')
+
+if(isMainThread){
+
+    const worker = new Worker(__filename)
+
+    worker.on('message',msg=>{console.log(msg)})
+
+}else{
+
+    parentPort.postMessage('hello world')
+}
