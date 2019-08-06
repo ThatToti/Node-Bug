43bb83cf859e9702dbe614d5af53ce6491b6069a
ThatToti <mr.totihuang@gmail.com>
2019-08-06 20:39:33 +0800

    [+]internal/worker.js add some const

---
+++ internal/worker.js
@@ -0,0 +1,56 @@
+
+const { Object }=primordials
+
+const EventEmitter = require('events')
+const assert = require('internal/assert')
+const path = require('path')
+const {parhToFileURL}=require('url')
+
+const {
+    ERR_WORKER_PATH,
+    ERR_WORKER_UNSERIALIZABLE_ERROR,
+    ERR_WORKER_UNSUPPORTED_EXTENSION,
+    ERR_WORKER_INVALID_EXEC_ARGV,
+    ERR_INVALID_ARG_TYPE,  
+} = require('internal/errors').codes
+const { deserializeError } = require('internal/error-serdes');
+
+const {valiadateString}=require('internal/validators')
+const { getOptionValue } = require('internal/options');
+
+const workerIo = require('internal/worker/io')
+
+const {
+    drainMessagePort,
+    MessageChannel,
+    messageTypes,
+    kPort,
+    kIncrementsPortRef,
+    kWaitingStreams,
+    kStdioWantsMoreDataCallback,
+    setupPortReferencing,
+    ReadableWorkerStdio,
+    WritableWorkerStdio
+} = workerIo
+
+const {
+    ownsProcessState,
+    isMainThread,
+    threadId,
+    Worker:WorkerImpl
+}=internalBinding('worker')
+
+const kHandle = Symbol('kHandle');
+const kPublicPort = Symbol('kPublicPort');
+const kDispose = Symbol('kDispose');
+const kOnExit = Symbol('kOnExit');
+const kOnMessage = Symbol('kOnMessage');
+const kOnCouldNotSerializeErr = Symbol('kOnCouldNotSerializeErr');
+const kOnErrorMessage = Symbol('kOnErrorMessage');
+const kParentSideStdio = Symbol('kParentSideStdio');
+
+const SHARE_ENV = Symbol.for('nodejs.worker_threads.SHARE_ENV');
+
+const debug = require('internal/util/debuglog').debuglog('worker')
+
+
