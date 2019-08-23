978df9881213fe4d4e9270fe2aecfb43209c6cc5
ThatToti <mr.totihuang@gmail.com>
2019-08-23 20:38:30 +0800

    [+]pass by reference

--- c++/invoking_func.cc
+++ c++/invoking_func.cc
@@ -1,38 +1,50 @@
 #include <iostream>
 using namespace std;
 
-void display(vector<int> vec){
-    for(int ix=0;ix<vec.size();++ix){
-        cout<<vec[ix]<<' ';
-        count<<endl;
-    }
-}
+// void display(vector<int> vec){
+//     for(int ix=0;ix<vec.size();++ix){
+//         cout<<vec[ix]<<' ';
+//         cout<<endl;
+//     }
+// }
 
-void swap(int val1,int val2){
+// void swap(int val1,int val2){
+//     int temp=val1;
+//     val1 = val2;
+//     val2 = temp;
+// }
+
+// void bubble_sort(vector<int> vec){
+//     for(int ix =0;ix<vec.size(); ++ix){
+//         for(int jx=ix+1;jx<vec.size();==jx){
+//             if(vec[ix]>vec[jx])
+//                 swap(vec[ix],vec[jx])
+//         }
+//     }
+// }
+
+void swap(int &val1,int &val2){
     int temp=val1;
-    val1 = val2;
-    val2 = temp;
-}
-
-void bubble_sort(vector<int> vec){
-    for(int ix =0;ix<vec.size(); ++ix){
-        for(int jx=ix+1;jx<vec.size();==jx){
-            if(vec[ix]>vec[jx])
-                swap(vec[ix],vec[jx])
-        }
-    }
+    val1=val2;
+    val2=temp;
 }
 
 int main(){
-    int ia[8]={8,34,3,13,1,21,5,2}
-    vector<int> vec(ia,ia+8)
+    // int ia[8]={8,34,3,13,1,21,5,2}
+    // vector<int> vec(ia,ia+8)
 
-    cout<<"vector before sort:";
-    display(vec)
+    // cout<<"vector before sort:";
+    // display(vec)
 
-    bubble_sort(vec)
+    // bubble_sort(vec)
 
-    cout<<"vector after sort:"
+    // cout<<"vector after sort:"
 
-    display(vec)
+    // display(vec)
+    int a=3,b=5;
+    swap(a,b);
+    cout<<"a:"<<a;
+    cout<<"b:"<<b;
+    cout<<endl;
+    return 0;
 }
