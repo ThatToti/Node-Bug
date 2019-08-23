cb34e05459d9ff32010e981943ad6424327497e4
ThatToti <mr.totihuang@gmail.com>
2019-08-23 22:15:32 +0800

    【+】using pointer

--- c++/invoking_func.cc
+++ c++/invoking_func.cc
@@ -15,12 +15,24 @@ void display(const vector<int> &vec){
     }
 }
 
+void display_pointer(const vector<int> *vec){
+    if(!vec){
+        cout<<"the vector pointer is 0\n";
+        return;
+    }
+    for(int ix=0;ix<vec->size();++ix){
+        cout<<(*vec)[ix]<<' ';
+        cout<<endl;
+    }
+}
+
 int main(){
     int ia[8]={8,34,3,13,1,21,5,2};
     vector<int> vec(ia,ia+8);
 
     cout<<"vector before sort:";
     display(vec);
+    display_pointer(&vec);
 
     // bubble_sort(vec)
 
